ae7fd9dac4ebb56e8d9167343adc9f2750c62771
YOUNTAEHEE <f_hyang@naver.com>
2024-10-20 21:03:55 +0900

    feat: logout 기능 추가

--- app/[locale]/(home)/mypage/page.tsx
+++ app/[locale]/(home)/mypage/page.tsx
@@ -4,44 +4,69 @@ import { Button } from "@nextui-org/button";
 import { Switch } from "@nextui-org/switch";
 import { Card, CardBody } from "@nextui-org/card";
 import { useTranslations } from 'next-intl';
+import { useState } from "react";
 import React from "react";
+import apiClient from "@handler/fetch/client";
+import { useRouter } from "next/navigation";
+import useLocaleStore from "@store/useLocaleStore";
+import useUserStore from "@store/useUserStore";
 
 export default function MyPage() {
   const t = useTranslations();  // 번역을 위한 useTranslations 훅 사용
-
+  const [error, setError] = useState<String | null>(null);
+  const router = useRouter();
+  const { locale, toggleLocale } = useLocaleStore();
+  const clearUserInfo = useUserStore((state) => state.clearUserInfo);
+  
+  const handleLogout = async() =>{
+    setError(null);
+    try{
+      const response = await apiClient.post("/auth/logout");
+      localStorage.removeItem("token");
+      localStorage.removeItem("successMessage");
+      clearUserInfo(); 
+      router.push(`/${locale}/login`);
+    }catch(error: any){
+        if (error.response && error.response.data && error.response.data.message) {
+          setError(error.response.data.message);
+        } else {
+          setError("로그아웃 실패");
+        }
+      }
+  };
   return (
     <div className="flex flex-col min-h-screen bg-gray-100">
-      <header className="bg-white p-4 sticky top-0 z-10 shadow-sm">
+      <header className="sticky top-0 z-10 p-4 bg-white shadow-sm">
         <h1 className="text-lg font-bold">{t('마이페이지')}</h1>
       </header>
 
-      <main className="flex-grow p-4 space-y-4 pb-16">
+      <main className="flex-grow p-4 pb-16 space-y-4">
         <Card>
           <CardBody className="p-4">
-            <div className="flex justify-between items-center">
+            <div className="flex items-center justify-between">
               <div>
                 <p className="text-sm text-gray-500">{t('닉네임')}</p>
-                <p className="font-bold text-lg">xxxx</p>
+                <p className="text-lg font-bold">xxxx</p>
               </div>
-              <Button className="bg-green-500 hover:bg-green-600 text-white">
+              <Button className="text-white bg-green-500 hover:bg-green-600">
                 {t('적립내역')}
               </Button>
             </div>
             <div className="mt-2">
               <p className="text-sm text-gray-500">{t('보유_캐시')}</p>
-              <p className="font-bold text-2xl">85 {t('캐시')}</p> 
+              <p className="text-2xl font-bold">85 {t('캐시')}</p> 
             </div>
           </CardBody>
         </Card>
 
         <Card>
           <CardBody className="p-4">
-            <div className="flex justify-between items-center">
+            <div className="flex items-center justify-between">
               <div>
                 <p className="text-sm text-gray-500">{t('추천해준_친구')}</p>
-                <p className="font-bold text-lg">0 {t('명')}</p>
+                <p className="text-lg font-bold">0 {t('명')}</p>
               </div>
-              <Button className="bg-green-500 hover:bg-green-600 text-white">
+              <Button className="text-white bg-green-500 hover:bg-green-600">
                 {t('추천인_내역')}
               </Button>
             </div>
@@ -52,7 +77,7 @@ export default function MyPage() {
           <CardBody className="p-4 space-y-4">
             <div className="flex items-center justify-between">
               <div className="flex items-center space-x-2">
-                <Bell className="h-5 w-5 text-gray-500" />
+                <Bell className="w-5 h-5 text-gray-500" />
                 <span>{t('푸시알림')}</span>
               </div>
               <Switch />
@@ -62,14 +87,16 @@ export default function MyPage() {
               { icon: FileText, label: t('공지사항') },
               { icon: Users, label: t('제휴광고') },
               { icon: HelpCircle, label: t('문의하기') },
-              { icon: LogOut, label: t("로그아웃") }, 
+              { icon: LogOut, label: t("로그아웃"), onClick: handleLogout }, 
             ].map((item, index) => (
-              <Button key={index} variant="light" className="w-full justify-between py-2 px-0">
+              <Button key={index} variant="light" className="justify-between w-full px-0 py-2"
+              onClick={item.onClick}
+              >
                 <div className="flex items-center space-x-2">
-                  <item.icon className="h-5 w-5 text-gray-500" />
+                  <item.icon className="w-5 h-5 text-gray-500" />
                   <span>{item.label}</span>
                 </div>
-                <ArrowRight className="h-5 w-5 text-gray-400" />
+                <ArrowRight className="w-5 h-5 text-gray-400" />
               </Button>
             ))}
           </CardBody>
